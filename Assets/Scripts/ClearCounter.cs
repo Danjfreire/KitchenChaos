5f3181ef9decb2a493d2e489f2182dcb9c9154b7
Daniel <danjfreire@gmail.com>
2024-11-26 08:44:28 -0300

    feat: moving objects between counters

--- Assets/Scripts/ClearCounter.cs
+++ Assets/Scripts/ClearCounter.cs
@@ -7,12 +7,47 @@ public class ClearCounter : MonoBehaviour
     [SerializeField] private KitchenObjectSO kitchenObjectSO;
     [SerializeField] private Transform counterTopPoint;
 
+    [SerializeField] private ClearCounter secondCounter;
+    [SerializeField] private bool testing;
+
+    private KitchenObject kitchenObject;
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.T) && testing) {
+            if (kitchenObject != null) {
+                kitchenObject.SetClearCounter(secondCounter);
+            }
+        }
+    }
+
     public void Interact()
     {
-        Debug.Log("Interacting");
-        Transform kitchenObjectTransform = Instantiate(kitchenObjectSO.prefab, counterTopPoint);
-        kitchenObjectTransform.localPosition = Vector3.zero;
+        if (kitchenObject == null) {
+            Transform kitchenObjectTransform = Instantiate(kitchenObjectSO.prefab, counterTopPoint);
+            kitchenObjectTransform.GetComponent<KitchenObject>().SetClearCounter(this);
+        } else {
+            Debug.Log(kitchenObject.GetClearCounter());
+        }
+    }
 
-        Debug.Log("Spawned a " + kitchenObjectTransform.GetComponent<KitchenObject>().GetKitchenObjectSO().objectName);
+    public bool HasKitchenObject()
+    {
+        return kitchenObject != null;
+    }
+
+    public Transform GetTopPoint()
+    {
+        return counterTopPoint;
+    }
+
+    public void ClearKitchenObject()
+    {
+        kitchenObject = null;
+    }
+
+    public void SetKitchenObject(KitchenObject kitchenObject)
+    {
+        this.kitchenObject = kitchenObject;
     }
 }
--- Assets/Scripts/KitchenObject.cs
+++ Assets/Scripts/KitchenObject.cs
@@ -6,8 +6,34 @@ public class KitchenObject : MonoBehaviour
 {
     [SerializeField] private KitchenObjectSO kitchenObjectSO;
 
+    private ClearCounter clearCounter;
     public KitchenObjectSO GetKitchenObjectSO()
     {
         return kitchenObjectSO;
+    }
+
+    public void SetClearCounter(ClearCounter clearCounter)
+    {
+        // Remove kitchen object from current clear counter if it exists
+        if (this.clearCounter != null) {
+            this.clearCounter.ClearKitchenObject();
+        }
+
+        // Add kitchen object to new clear counter
+        if (clearCounter.HasKitchenObject()) {
+            // this should never happen!
+            Debug.LogError("Clear counter already has a kitchen object");
+        }
+
+        this.clearCounter = clearCounter;
+        this.clearCounter.SetKitchenObject(this);
+
+        transform.parent = clearCounter.GetTopPoint();
+        transform.localPosition = Vector3.zero;
+    }
+
+    public ClearCounter GetClearCounter() 
+    {
+        return this.clearCounter;
     }
 }
